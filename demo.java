612eada93203a2a8afa6a1c8fe9edca2ef840448
Jasper Binda <jasper.binda@youngcolfield.nl>
2018-02-02 09:32:10 +0100

    HelloWorld

--- demo.java
+++ demo.java
@@ -1,5 +1,29 @@
 class Demo{
 	public static void main(String[] args){
+		House h1 = new House();
+		h1.housenumber = 34;
+		h1.name = "Street";
 
+		street(h1.name);
+
+		nextnumber(h1);
+		//nextnumber(h1.housenumber);
+
+		System.out.println("name " + h1.name);
+		System.out.println("next number " + h1.housenumber);
 	}
+	static void street(String x){
+		x = "new street";
+	}
+	
+	static void nextnumber(House x){
+	//static void nextnumber(int x){
+		x.housenumber+=2;
+	//	x+=2;
+	}
+}
+
+class House{
+	int housenumber;
+	String name;
 }
